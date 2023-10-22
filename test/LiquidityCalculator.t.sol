b67cd63448ff2a024797f7fc126c8bc24dd6da0d
Shresth Verma <shresthv1@gmail.com>
2023-10-22 22:26:34 +0530

    UpdatedLiquidityCalculator

--- test/LiquidityCalculator.t.sol
+++ test/LiquidityCalculator.t.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {LiquidityCalculator} from "../src/LiquidityCalculator.sol";
 
 contract LiquidityCalculatorTest is Test {
     LiquidityCalculator public liquidityCalculator;
+    
+    event DebugLogUInt128(uint128 value);
 
     function setUp() public {
         liquidityCalculator = new LiquidityCalculator();
     }
 
-
     function test_LiquidityForScenario1() public {
         uint128 liquidity = liquidityCalculator.getLiquidityForScenario1();
+        emit DebugLogUInt128(liquidity); // Log the value using an event
         assertEq(liquidity, 1.3e21);
     }
 }
