ee7c8750f997289f1b1d3e6ce710dcc30349437b
Shresth Verma <shresthv1@gmail.com>
2023-10-23 15:06:29 +0530

    addedThe2ndLiquidity

---
+++ src/LiquidityCalbyMuldiv.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "v4-core/interfaces/IPoolManager.sol";
+import "periphery-next/libraries/LiquidityAmounts.sol";
+import {FullMath} from "@uniswap/v4-core/contracts/libraries/FullMath.sol";
+
+contract LiquidityCalculatorByMulDiv {
+    function getLiquidityForAmount0(
+        uint160 sqrtPriceAX96,
+        uint160 sqrtPriceBX96,
+        uint256 amount0
+    ) internal pure returns (uint128 liquidity) {
+        if (sqrtPriceAX96 > sqrtPriceBX96)
+            (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
+
+        uint256 intermediate = FullMath.mulDiv(
+            sqrtPriceAX96,
+            sqrtPriceBX96,
+            FixedPoint96.Q96
+        );
+        liquidity = uint128(
+            FullMath.mulDiv(
+                amount0,
+                intermediate,
+                sqrtPriceBX96 - sqrtPriceAX96
+            )
+        );
+    }
+
+    function getLiquidityForAmount1(
+        uint160 sqrtPriceAX96,
+        uint160 sqrtPriceBX96,
+        uint256 amount1
+    ) internal pure returns (uint128 liquidity) {
+        if (sqrtPriceAX96 > sqrtPriceBX96)
+            (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
+
+        liquidity = uint128(
+            FullMath.mulDiv(
+                amount1,
+                FixedPoint96.Q96,
+                sqrtPriceBX96 - sqrtPriceAX96
+            )
+        );
+    }
+
+    function getLiquidityForAmounts(
+        uint160 sqrtPriceX96,
+        uint160 sqrtPriceAX96,
+        uint160 sqrtPriceBX96,
+        uint256 amount0,
+        uint256 amount1
+    ) internal pure returns (uint128 liquidity) {
+        if (sqrtPriceAX96 > sqrtPriceBX96)
+            (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
+
+        if (sqrtPriceX96 <= sqrtPriceAX96) {
+            liquidity = getLiquidityForAmount0(
+                sqrtPriceAX96,
+                sqrtPriceBX96,
+                amount0
+            );
+        } else if (sqrtPriceX96 <= sqrtPriceBX96) {
+            uint128 liquidity0 = getLiquidityForAmount0(
+                sqrtPriceX96,
+                sqrtPriceBX96,
+                amount0
+            );
+            uint128 liquidity1 = getLiquidityForAmount1(
+                sqrtPriceAX96,
+                sqrtPriceX96,
+                amount1
+            );
+
+            liquidity = liquidity0 < liquidity1 ? liquidity0 : liquidity1;
+        } else {
+            liquidity = getLiquidityForAmount1(
+                sqrtPriceAX96,
+                sqrtPriceBX96,
+                amount1
+            );
+        }
+    }
+}
